0199c63910f05f7bda7bf252f930449900d485eb
kbagot <kbagot@student.42.fr>
2016-12-17 15:22:19 +0100

    done

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kbagot <kbagot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/07 14:37:51 by kbagot            #+#    #+#             */
-/*   Updated: 2016/12/16 16:47:48 by kbagot           ###   ########.fr       */
+/*   Updated: 2016/12/16 19:55:21 by kbagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ static int		ft_makeline(char *lst, char **line)
 		lst = ft_strcpy(lst, &lst[i + 1]);
 		if (fin == 1)
 			lst[0] = '\0';
-//		printf("reste2 :%s\n", lst);
 		return (1);
 	}
 	return (0);
@@ -61,45 +60,55 @@ static t_list	*ft_find_fd(t_list *rstr, int fd)
 	new = ft_lstnew(NULL, fd);
 	new->content_size = fd;
 	new->content = ft_strnew(BUFF_SIZE);
-//	ft_lstadd(&saverstr, new);
 	new->next = NULL;
 	rstr->next = new;
 	return (new);
 }
 
-int				get_next_line(const int fd, char **line)
+static int		ft_stock(t_list *rstr, int fd)
 {
-	char			*buff;
-	static t_list	*rstr = NULL;
-	t_list			*saverstr;
-	char			*stop;
-	int				ret;
+	char	*buff;
+	char	*stop;
+	int		ret;
+	char	*test;
 
-	saverstr = NULL;
 	stop = NULL;
-	if (fd < 0 || !line)
-		return (-1);
-	saverstr = rstr;
-	rstr = ft_find_fd(rstr, fd);
-//	printf("---\nRest :%s\n", rstr->content);
 	buff = ft_strnew(BUFF_SIZE);
+	test = rstr->content;
 	while (stop == NULL && (ret = read(fd, buff, BUFF_SIZE)))
 	{
 		if (ret == -1)
 			return (-1);
 		ft_bzero(&buff[ret], BUFF_SIZE - ret);
 		stop = ft_strchr(buff, '\n');
-		rstr->content = ft_strjoin(rstr->content, buff);
+		rstr->content = ft_strjoin(test, buff);
+		free(test);
+		test = rstr->content;
 	}
 	free(buff);
 	buff = NULL;
+	return (0);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static t_list	*rstr = NULL;
+	t_list			*saverstr;
+
+	saverstr = NULL;
+	if (fd < 0 || !line)
+		return (-1);
+	saverstr = rstr;
+	rstr = ft_find_fd(rstr, fd);
+	if (ft_stock(rstr, fd) == -1)
+		return (-1);
 	if (ft_makeline(rstr->content, line))
 	{
-		if (saverstr != NULL)
-		rstr = saverstr;
+		if (saverstr)
+			rstr = saverstr;
 		return (1);
 	}
-		if (saverstr != NULL)
+	if (saverstr)
 		rstr = saverstr;
 	return (0);
 }
